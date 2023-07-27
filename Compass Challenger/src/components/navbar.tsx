e29adc521f5159a05d79d823228bd533c5d8ba07
gpurplem <megabrielbm@gmail.com>
2023-07-27 14:19:55 -0300

    Navbar layout

--- Compass Challenger/src/components/navbar.tsx
+++ Compass Challenger/src/components/navbar.tsx
@@ -10,22 +10,24 @@ const Navbar: React.FC = () => {
 
   return (
     <nav className="navbar">
-      <div className="nav-group-left">
-        <img className='img-nav' src={logo} alt="Logo" onClick={() => navigate('/')}/>
-        <span className="nav-link">Início</span>
-        <span className="nav-link profile-link">Perfil</span>
-        <span className="nav-link">Comunidades</span>
-        <span className="nav-link">Jogos</span>
-      </div>
-      <div className="nav-group-right">
-        <div className="search-bar-wrapper">
-          <img src={searchIcon} alt="Pesquisar" className="search-icon" />
-          <input type="search" placeholder="Pesquisar no Orkut" className="search-bar"/>
+      <div className='navbar-content'>
+        <div className="nav-group-left">
+          <img className='img-nav' src={logo} alt="Logo" onClick={() => navigate('/')} />
+          <span className="nav-link">Início</span>
+          <span className="nav-link profile-link">Perfil</span>
+          <span className="nav-link">Comunidades</span>
+          <span className="nav-link">Jogos</span>
         </div>
-        <div className="user-profile">
-          <img src={userImage} alt="Usuário" width="20" height="20" />
-          <span className="user-name">Iuri Silva</span>
-          <i className="arrow-down"></i>
+        <div className="nav-group-right">
+          <div className="search-bar-wrapper">
+            <img src={searchIcon} alt="Pesquisar" className="search-icon" />
+            <input type="search" placeholder="Pesquisar no Orkut" className="search-bar" />
+          </div>
+          <div className="user-profile">
+            <img src={userImage} alt="Usuário" width="20" height="20" />
+            <span className="user-name">Iuri Silva</span>
+            <i className="arrow-down"></i>
+          </div>
         </div>
       </div>
     </nav>
