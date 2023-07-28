5aeec55a761d079f475575345f9234740b1c5f1a
geleco <rangel001@gmail.com>
2023-07-28 15:25:41 -0300

    task: navbar mobile

--- Compass Challenger/src/components/navbar.tsx
+++ Compass Challenger/src/components/navbar.tsx
@@ -9,28 +9,34 @@ const Navbar: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <nav className="navbar">
-      <div className='navbar-content'>
-        <div className="nav-group-left">
-          <img className='img-nav' src={logo} alt="Logo" onClick={() => navigate('/')} />
-          <span className="nav-link">Início</span>
-          <span className="nav-link profile-link">Perfil</span>
-          <span className="nav-link">Comunidades</span>
-          <span className="nav-link">Jogos</span>
-        </div>
-        <div className="nav-group-right">
-          <div className="search-bar-wrapper">
-            <img src={searchIcon} alt="Pesquisar" className="search-icon" />
-            <input type="search" placeholder="Pesquisar no Orkut" className="search-bar" />
+    <>
+      <nav className="navbar">
+        <div className='navbar-content'>
+          <div className="nav-group-left">
+            <img className='img-nav' src={logo} alt="Logo" onClick={() => navigate('/')} />
+            <span className="nav-link">Início</span>
+            <span className="nav-link profile-link">Perfil</span>
+            <span className="nav-link">Comunidades</span>
+            <span className="nav-link">Jogos</span>
           </div>
-          <div className="user-profile">
-            <img src={userImage} alt="Usuário" width="20" height="20" />
-            <span className="user-name">Iuri Silva</span>
-            <i className="arrow-down"></i>
+          <div className="nav-group-right">
+            <div className="search-bar-wrapper">
+              <img src={searchIcon} alt="Pesquisar" className="search-icon" />
+              <input type="search" placeholder="Pesquisar no Orkut" className="search-bar" />
+            </div>
+            <div className="user-profile">
+              <img src={userImage} alt="Usuário" width="20" height="20" />
+              <span className="user-name">Iuri Silva</span>
+              <i className="arrow-down"></i>
+            </div>
           </div>
         </div>
+      </nav>
+      <div className="search-bar-mobile-wrapper">
+        <img src={searchIcon} alt="Pesquisar" className="search-icon" />
+        <input type="search" placeholder="Pesquisar no Orkut" className="search-bar" />
       </div>
-    </nav>
+    </>
   );
 };
 
